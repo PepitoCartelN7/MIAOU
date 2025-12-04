bdfdf2efc11ab692144460301f89a4f09498ef4e
QuoicouLC <bbalc.olivier@gmail.com>
2025-12-04 15:05:45 +0100

    makefile ajouté

--- TestDiscord/src/MP3Player.java
+++ TestDiscord/src/MP3Player.java
@@ -1,9 +1,14 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
  
 public class MP3Player {
+
+    private ArrayList<String> playlist = new ArrayList<>();
+
     public void play(String filename)  {
         try {
              Player player = new Player(new FileInputStream(filename)); // Creating a player
@@ -12,6 +17,27 @@ public class MP3Player {
             e.printStackTrace();
         }
     }
+
+
+    public void play_list(){
+
+        String current;
+        playlist.add("assets/victory.mp3");
+        playlist.add("assets/fly-me-to-the-moon-climax.mp3");
+
+        
+        while (!playlist.isEmpty()) {
+
+            current = playlist.getFirst();
+            System.out.println("PROUT1");
+            play(current);
+            playlist.remove(current);
+            System.out.println("PROUT2");
+
+        }
+     };
+
+    
 }
 
     
