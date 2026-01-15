95bec1b5a5ad263c4743f6c97c022893eb61e462
pepito <pepito.cartel.n7@gmail.com>
2026-01-15 13:54:23 +0100

    git c' 'esst de la merd je vais tous casser

---
+++ MIAOU_Bot/src/miaou.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import subprocess
+import time
+
+portled = 21
+portcapteur = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(portled,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(portcapteur,GPIO.IN,pull_up_down = GPIO.PUD_UP)
+print("super")
+allume = False
+GPIO.output(portled,GPIO.LOW)
+print("debut compilation")
+#proc = subprocess.Popen("make compile", shell=True)
+#proc.wait()
+print("fin compilation")
+pid = 0;
+while True:
+    if (GPIO.input(portcapteur)==0 and allume):
+        GPIO.output(portled,GPIO.LOW)
+        print("detection fermeture")
+        print(pid)
+        proc = subprocess.Popen("killall java", shell=True)
+        allume = False
+        
+    elif (not allume and GPIO.input(portcapteur)==1):
+        GPIO.output(portled,GPIO.HIGH)
+        allume = True
+        proc = subprocess.Popen("make run", shell=True)
+        print("detection ouverture")
+    time.sleep(1)
+    print(GPIO.input(portcapteur))
+    print(allume)
